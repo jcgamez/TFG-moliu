47f9c0855457c82251fda238135ad51ba700b5c0
padrazo04 <padrazo04@gmail.com>
2022-03-12 20:01:11 +0100

    initial: first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Moliu. Software for gross motor skills treatment
+
+## Web application
+
+This webapp is part of **Moliu** project. Its objective is the patient, activities, plays and machine learning models management.
+
+Managed entities:
+* Patient: User who performs activities
+* Specialist: User responsible for managing Moliu software
+* Activities: Interactive tasks proposed to patients
+* Play: Activity performed by a patient
---
+++ commitlint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "type-enum": [
+        2,
+        "always",
+        [
+          "assets",
+          "build",
+          "chore",
+          "ci",
+          "config",
+          "docs",
+          "feat",
+          "fix",
+          "initial",
+          "perf",
+          "refactor",
+          "remove",
+          "rename",
+          "revert",
+          "style",
+          "test",
+        ],
+      ],
+      "scope-enum": [
+        2,
+        "always",
+        [
+          "activity",
+          "design",
+          "patient",
+          "security",
+          "specialist",
+          "play",
+          "ia",
+        ]
+    ],
+    },
+  };
